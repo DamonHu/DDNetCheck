d9d06b54fcf23d543a8b136e408bac53efba9f2b
Damon <dong765@qq.com>
2025-04-15 10:53:10 +0800

    Update DDNetCheckVC.swift

--- Pods/DDNetCheckVC.swift
+++ Pods/DDNetCheckVC.swift
@@ -16,10 +16,16 @@ func UIImageHDBoundle(named: String?) -> UIImage? {
 
 extension String{
     var ZXLocaleString: String {
-        guard let bundlePath = Bundle(for: DDNetCheck.self).path(forResource: "DDNetCheck", ofType: "bundle") else { return NSLocalizedString(self, comment: "") }
-        guard let bundle = Bundle(path: bundlePath) else { return NSLocalizedString(self, comment: "") }
-        let msg = NSLocalizedString(self, tableName: nil, bundle: bundle, value: "", comment: "")
-        return msg
+        //优先使用主项目翻译
+        let mainValue = NSLocalizedString(self, comment: "")
+        if mainValue != self {
+            return mainValue
+        }
+        //使用自己的bundle
+        if let bundlePath = Bundle(for: DDNetCheck.self).path(forResource: "DDNetCheck", ofType: "bundle"), let bundle = Bundle(path: bundlePath) {
+            return NSLocalizedString(self, tableName: nil, bundle: bundle, value: self, comment: "")
+        }
+        return self
     }
 }
 
